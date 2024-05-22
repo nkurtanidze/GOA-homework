179a6774ed6fe960223a534b2e78397c4997d331
nkurtanidze <nikolozikurtanidze04@gmail.com>
2024-05-22 17:30:10 +0400

    fourth commit

---
+++ level4/homework/level4.py
@@ -0,0 +1,311 @@
+from turtle import *
+
+speed(30)
+width(5)
+forward (300)
+left(90)
+
+forward (200)
+left(90)
+
+forward (30)
+left(90)
+
+forward (30)
+right(90)
+
+forward (30)
+right(90)
+
+forward (30)
+left(90)
+forward (30)
+right(90)
+
+forward (60)
+right(90)
+
+forward (30)
+left(120)
+
+forward (90)
+left(120)
+
+forward (90)
+left(120)
+
+forward (30)
+right(90)
+
+forward (30)
+right(90)
+
+forward (250)
+right(90)
+
+forward (30)
+right(90)
+
+forward (30)
+left(120)
+
+forward (90)
+left(120)
+
+forward (90)
+left(120)
+
+forward (30)
+right(90)
+
+forward (60)
+right(90)
+
+forward (30)
+left(90)
+
+forward (30)
+right(90)
+
+forward (30)
+right(90)
+
+forward (30)
+left(90)
+
+forward (30)
+left(90)
+
+forward (200)
+left(90)
+
+forward (200)
+left(90)
+
+forward (70)
+right(90)
+
+forward (50)
+right(90)
+
+forward (70)
+penup()
+goto(100,230)
+pendown()
+left(180)
+forward (120)
+left(90)
+
+forward (100)
+left(90)
+
+forward (90)
+left(90)
+
+forward (100)
+penup()
+goto(0,330)
+pendown()
+forward (15)
+left(180)
+
+forward(15)
+left(90)
+
+forward(60)
+left(90)
+
+forward(15)
+left(90)
+
+forward (15)
+left(180)
+
+forward(15)
+right(90)
+
+forward(15)
+left(90)
+
+forward (10)
+left(90)
+
+forward (30)
+left(90)
+
+forward (60)
+right(90)
+
+forward(15)
+right(90)
+
+forward (60)
+left(90)
+
+forward (15)
+left(60)
+
+forward (75)
+left(200)
+
+forward (65)
+left(180)
+
+forward (25)
+left(90)
+forward(20)
+
+penup()
+goto(0,-20)
+pendown()
+left(100)
+forward (30)
+left(90)
+
+forward (40)
+left(90)
+
+forward (30)
+left(90)
+
+forward (40)
+right(90)
+
+forward (10)
+right(140)
+
+forward (15)
+left(140)
+
+forward (10)
+right(140)
+
+forward(10)
+left(140)
+
+forward (10)
+right(140)
+
+forward (10)
+left(140)
+
+forward(10)
+right(140)
+
+forward(15)
+left(140)
+
+forward(10)
+right(140)
+forward (15)
+
+right(45)
+forward (25)
+
+right(90)
+forward (10)
+
+penup()
+goto(30,-30)
+pendown()
+forward(2)
+
+penup()
+goto(10,-30)
+pendown()
+forward(2)
+
+penup()
+goto(10,-50)
+pendown()
+left(95)
+forward(100)
+
+left(60)
+forward (40)
+
+right(180)
+forward(40)
+left(60)
+forward(40)
+
+right(180)
+forward (40)
+
+left(60)
+forward (60)
+
+left(90)
+forward (40)
+
+right(180)
+forward (80)
+
+left(90)
+forward (10)
+right(90)
+forward (10)
+right(90)
+forward (10)
+left(90)
+
+forward (80)
+right(180)
+
+forward (40)
+left(90)
+
+forward (60)
+left(60)
+
+forward (40)
+right(180)
+
+forward (40)
+left(60)
+
+forward (40)
+right(180)
+forward (40)
+
+left(60)
+forward (100)
+
+left(90)
+forward (10)
+
+right(90)
+forward (30)
+
+right(90)
+forward (30)
+
+right(90)
+forward (30)
+
+right(90)
+forward (30)
+
+right(90)
+forward (10)
+
+right(90)
+forward (10)
+
+left(180)
+forward (10)
+
+right(90)
+forward (10)
+right(90)
+penup()
+goto(100,-30)
+pendown()
+forward (1)
+
+penup()
+goto(104,-30)
+pendown()
+forward (1)
+
+exitonclick()
