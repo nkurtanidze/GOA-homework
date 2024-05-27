05d7cedf604eb5b2cf9e7e4321e9f730102006d9
nkurtanidze <nikolozikurtanidze04@gmail.com>
2024-05-27 21:55:53 +0400

    fifth commit

---
+++ level5/homework/level5.py
@@ -0,0 +1,26 @@
+# დავალებე 2
+
+# შევქმენი ცვლადები სადაც შევინახე ჩემს თავზე ინფორმაცია
+name = "nika"
+surname = "kurtanidze"
+age = 15 
+address = "gumathesis street"
+print("I am " + name + " " + surname + " " + str(age) + " years old " + " I live " + address)
+
+# დავალება 3
+
+# შევქმენი 10 ცვლადი და მათზე გამოვიენე მათემატიკუტი ოპერაცია
+num1 = 1
+num2 = 2
+num3 = 3
+num4 = 4
+num5 = 5
+num6 = 6
+num7 = 7
+num8 = 8
+num9 = 9
+num10 =10
+print(num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10)  # შევკრიბე რიცხვები
+print(num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10)  # გამოვაკელი რიცხვები
+print(num1 / num2 / num3 / num4 / num5 / num6 / num7 / num8 / num9 / num10)  # გავყავი რიცხვები
+print(num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10)  # გავამრავლე რიცხვები
