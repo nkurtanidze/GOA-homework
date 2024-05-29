19957502d5e48ec11a20cb648e101d2427872f93
nkurtanidze <nikolozikurtanidze04@gmail.com>
2024-05-29 17:46:15 +0400

    six commit

--- level6/calsswork/level6.py
+++ level6/calsswork/level6.py
@@ -1,3 +1,14 @@
 num1 = input()
 num2 = input()
 print(int(num1) + int(num2))
+
+
+
+
+
+# davaleba 2
+firstname = input("please enter your firstname:nika ")
+lastname = input("please enter your lastname:kurtanidze")
+age = input("please enter your age:15")
+email = input("please enter your email:nikolozikurtanidze04@gmail.com")
+print("your information is + firstname + lastname + int(age) + email")
---
+++ level6/homework/level6.py
@@ -0,0 +1,28 @@
+# დავალება 2
+# num1 = int(input("please enter your num1:"))
+# print(num1)
+# num2 = int(input("please enter your num2:"))
+# print(num2)
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 / num2)
+# print(num1 * num2)
+
+
+# დავალება 3
+# name = input("please enter your name:")
+# print("happy birhday " + name)
+
+# დავალება 4
+# name = input("please enter your name:")
+# surname = input("please enter your surname:")
+# age = input("please enter your age:")
+# print(name + " " + surname + " " + age)
+
+# დავალება 5
+deda = int(input("please enter your age:"))
+mama = int(input("please enter your age:"))
+da = int(input("please enter your age:"))
+print(deda + 25)
+print(mama + 25)
+print(da + 25)
