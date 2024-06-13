a672422af54bfb9802efab04a6095528844805fa
nkurtanidze <nikolozikurtanidze04@gmail.com>
2024-06-13 08:11:04 +0400

    ten commit

---
+++ level10/classwork/level10.py
@@ -0,0 +1,12 @@
+# 2 სახის ლუპი for / whille loop
+
+# for loop
+# i არის საიტერაციო ცვლადი  iteration / განმეორება
+# i in range(100) / იტერაცია განმეორდეს 100-ჯერ
+
+# iteration - 4 space (tab) / iteration - განმეპრება
+# იტერაცია იწყება 0-დან და არის integer
+
+
+for i in range(100):
+    print(str(i) + "." "I love akhalcixe")
---
+++ level10/homework/level10.py
@@ -0,0 +1,30 @@
+# დავალება 
+for i in range(100):
+    print(str(i) + "." + "goa is the best")
+
+for i in range(100 + 1):
+    print(str(i) + "." + "I love akhaltsikhe")
+
+for i in range(100):
+    print(str(i) + "." "best mentor is batoni luka tsxvaradze")
+
+for i in range(100):
+    print(str(i) + "." + "Georgia is the best country in the world")
+
+for i in range(100 + 1):
+    print(str(i) + "." + "mtsvadi is the best food")
+
+for i in range(100):
+    print(str(i) + "." + "my name is nika")
+
+for i in range(100):
+    print(str(i) + "." + "my surname is kurtanidze")
+
+for i in range(100):
+    print(str(i) + "." + "I am fiftin years old")
+
+for i in range(100):
+    print(str(i) + "." + "I love Goa")
+
+for i in range(100):
+    print(str(i) + "." "I lone Georgia")
