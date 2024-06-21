cf04d5903755ce494715a687f1275d8540aa9f46
nkurtanidze <nikolozikurtanidze04@gmail.com>
2024-06-21 16:31:07 +0400

    twelve commit

---
+++ level12/homework/level12.py
@@ -0,0 +1,92 @@
+# # for loop
+# # 1
+# for i in range(1):
+#     print(i)
+# for i in range(2):
+#     print(i)
+# for i in range(3):
+#     print(i)
+# for i in range(4):
+#     print(i)
+# for i in range(5):
+#     print(i)
+# for i in range(6):
+#     print(i)
+# for i in range(7):
+#     print(i)
+# for i in range(8):
+#     print(i)
+# for i in range(9):
+#     print(i)
+# for i in range(10):
+#     print(i)
+
+# # 2
+# for i in range(100):
+#     print(str(i) + "." + "Goa is the best")
+
+
+# # while loop
+# # 1
+# age = 1
+# while age < 6:
+#     print(age)
+#     age = age + 1
+
+# # 2
+# age = 1
+# while age < 4:
+#     print("hello")
+#     age = age + 1
+
+
+# # conditionals statements
+# # Ease
+# # 1
+# age = 18
+# if age < 18:
+#     print("discount")
+# else:
+#     print("regular price")
+
+# # 2
+# age = 1
+# if age == 10:
+#     print("discount")
+# else:
+#     print("regular price")
+
+
+# # Hard
+# # 3
+# age = 5
+# if age > 0 and age < 10:
+#     print("child")
+# else:
+#     print("adult")
+
+# extreme
+# 4
+a = 7
+b = 10
+c = 5
+formula:"a +""+ "" b > c"  "a + c > b"   "b + c >a"
+a + b > c
+7 + 10 > 5
+17 > 5
+
+a + c > b
+7 + 5 > 10
+12 > 10
+
+b + c > a
+10 + 5 > 7
+15 > 7
+
+7,10,5 ("makes a triagles")
+
+5 + 8 > 3
+13 > 3 ("one side passes")
+
+5 + 3 > 8
+8 > 8 ("invalid")
