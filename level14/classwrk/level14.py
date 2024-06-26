66112767046d6072a8a3e8764b09f86290287260
nkurtanidze <nikolozikurtanidze04@gmail.com>
2024-06-26 10:34:47 +0400

    fourthin commit

---
+++ level14/classwrk/level14.py
@@ -0,0 +1,77 @@
+# davaleba 1
+sum = 0
+for i in range(0, 10+1, 2):
+    sum=sum + i
+    print(sum)
+
+# davaleba 2
+sum = 0
+for i in range(1, 9 +1, 2):
+    sum = sum + 1
+    print(sum)
+
+# davaleba 3
+sum = 0
+for i in range(5, 101, 5):
+    sum= sum + 1
+    print(sum)
+
+# davaleba 4
+for i in range(3, 101, 3):
+    print(i)
+
+# davaleba 5
+for i in range(20, 0, -1):
+    print(i)
+
+# davaleba 6
+sum = 0
+num1 = 1
+num2 = 10
+for i in range(1, 10):
+    sum = sum + 1
+    print(sum)
+
+
+
+
+
+# davaleba 1
+sum = 0
+age = 0
+while age <= 0:
+    sum = sum + 2
+    print(sum)
+
+# davaleba 2
+sum = 0
+age = 1
+while age <= 1:
+    sum = sum + 1
+    print(sum)
+
+# davaleba 3
+sum = 0
+age = 5
+while age <= 5:
+    sum = sum + 5
+    print(sum)
+
+# davaleba 4
+sum = 0
+age = 3
+while age <= 3:
+    sum = sum + 3
+    print(sum)
+
+# davaleba 5
+sum = 0
+age = 20
+while age <= 20:
+    sum = sum - 1
+    print(sum)
+
+# davaleba 6
+password = 1234567890
+while password <= 9876543210:
+    print(password)
---
+++ level14/homework/level14.py
@@ -0,0 +1,200 @@
+# davaleba 1
+# for loop
+# 1
+for i in  range(0, 20 + 1):
+    print(i)
+
+# 2
+for i in range(1, 10):
+    print(i)
+
+# 3
+for i in range(0, 101, 2):     # luwi
+    print(i)
+
+for i in range(1, 101, 2):    # kenti
+    print(i)
+
+
+# 4
+age = 10
+sum = 0
+for i in range(0, 10):
+    sum = sum + i
+    print(sum)
+
+
+# 5
+for i in range(5, 51, 5):
+    print(i)
+
+
+
+
+
+# while loop
+# 1
+age = 0
+while age < 20:
+    print(age)
+    age += 2
+
+
+# 2
+age = 1
+while age < 10:
+    print(age)
+
+
+# 3
+number = 7
+while int(input("please enter number")):
+    if number > 0 and number < 10:
+        print("you win !")
+    else:
+        print("please try again")
+
+
+# 5
+password = "password123"
+while input("please enter password"):
+    if password == "password123":
+        print("you win !")
+    else:
+        print("please try again")
+
+
+
+
+# if - else
+# 1
+clock = 12
+if clock < 12:
+    print("good morning")
+else:
+    print("good afternoon")
+
+
+# 2
+num = 45
+if num == "even":
+    print("odd")
+else:
+    print("even")
+
+
+# 3
+temperature = 30
+if temperature > 30:
+    print("hot outside !")
+else:
+    print("it's not to hot !")
+
+
+# 4
+score = 50
+if score >= 50:
+    print("passed")
+else:
+    print("failed")
+
+
+# 5
+age = 13
+if age >= 13 and age < 19:
+    print("you are a teenager")
+else:
+    print("you are not a teenager")
+
+
+
+
+# davaleba 2
+# for loop
+# 1
+for i in range(1, 51, 2):
+    print(i)
+
+
+# 2
+for i in range(2, 15, 2):
+    print(i)
+
+for i in range(3, 15, 3):
+    print(i)
+
+for i in range(4, 15, 4):
+    print(i)
+
+for i in range(5, 15, 5):
+    print(i)
+
+for i in range(6, 15, 6):
+    print(i)
+
+for i in range(7, 15, 7):
+    print(i)
+
+for i in range(8, 15, 8):
+    print(i)
+
+for i in range(9, 15, 9):
+    print(i)
+
+for i in range(10, 15, 10):
+    print(i)
+
+for i in range(11, 15, 11):
+    print(i)
+
+for i in range(12, 15, 12):
+    print(i)
+
+for i in range(13, 15, 13):
+    print(i)
+
+for i in range(14, 15, 14):
+    print(i)
+
+
+# 3
+num = 3
+for i in range(3):
+    print(i)
+
+
+# 4
+for i in range(3, 101, 3):
+    print(i)
+
+for i in range(5, 101, 5):
+    print(i)
+
+
+
+
+
+# while loop
+# 1
+num = 0
+while num < 50:
+    print(num)
+
+
+# 3
+sum = 0
+num = 1
+while num <= 10:
+    sum = sum + num
+    print(sum)
+
+
+
+
+# if - else
+# 1
+year = 2008
+if year <= 200000:
+    print("YES")
+else:
+    print("NO")
