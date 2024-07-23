5ceec4b98d5e59d9eb60ef2a6bd26d1201300cc4
nkurtanidze <nikolozikurtanidze04@gmail.com>
2024-07-23 10:58:59 +0400

    twentyonecommit

--- level20/homework/level20.py
+++ level20/homework/level20.py
@@ -1,28 +1,90 @@
 # 1. Write a function that takes a list of numbers and returns the sum of all the even numbers.
-# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# def number(x):
-#     even_sum = 0
-#     for i in x(1, 10, 2):
-#         if i % 2 == 0:
-#           even_sum += 1
-#         print(even_sum)
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+def number(x):
+    even_sum = 0
+    for i in (x):
+        if i % 2 == 0:
+          even_sum += i
+    print(even_sum)
 
-# sum(numbers)
+number(numbers)
+
 
 # 2. Create a function that accepts a string and returns the string reversed without using any built-in reverse functions.
+def reversed_string(string):
+    print(string[::-1])
+reversed_string("nika")
+
 
 # 3. Implement a function that takes a number and returns the factorial of that number using recursion.
+def fact(n):
+    if n == 1:
+        return 1
+    return fact(n - 1) * n
+
+print(fact(5))
+
 
 # 4. Design a function that accepts two lists and returns a list of common elements without using set operations.
+num1 = [1, 2, 3, 4, 5]
+num2 = [3, 5, 7, 9]
+def comon_nums(list1, list2):
+    result = []
+    for i in list1:
+       for j in list2:
+           print(f"i {i} j {j}")
+           if i == j:
+               result.append(i)
+    print(result)
+comon_nums(num1, num2)
+
 
 # 5. Write a function that takes a string and returns the number of vowels in the string.
+vowels = ["a", "e", "u", "o", "i"]
+string = "nika"
+def vowels_in_string(vowelss, name):
+    count = 0
+    for i in name:
+        for j in vowels:
+            if i == j:
+                count += 1
+    print(count)
+vowels_in_string(vowels, string)
+
 
 # 6. Create a function that accepts a list of integers and returns the list sorted in ascending order without using built-in sort functions.
 
 # 7. Implement a function that takes two strings and returns True if one string is a permutation of the other.
+name = "nika"
+names = "kian"
+def permutation(name, names):
+    count = 0
+    for i in name:
+         for j in names:
+              if i == j:
+                   count += 1
+    if count == len(name) and len(names) == len(name):
+         print(True)
+    else:
+         print(False)
+        
+permutation(name, names)
 
-# 8. Write a function that accepts a matrix (list of lists) and returns its transpose.
 
-# 9. Create a function that takes a number and returns True if it is a prime number.
+# 8. Create a function that takes a number and returns True if it is a prime number.
+def prime_number(x):
+    if x <= 1:
+        print(False)
+    for i in range(2, int((x ** 0.5)) + 1):
+            if x % i == 0:
+                return False
+            else:
+                return True
+print(prime_number(17))
 
-# 10. Design a function that accepts a list of strings and returns a list with the strings sorted by their lengths.
+
+# 9. Design a function that accepts a list of strings and returns a list with the strings sorted by their lengths.
+string = ["nika", "gio", "lukaa"]
+def names(list):
+    print(sorted(list, key=len))
+names(string)
