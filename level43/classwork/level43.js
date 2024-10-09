1d7d243e39b4e816f993b592b385e5cc3405aa78
nkurtanidze <nikolozikurtanidze04@gmail.com>
2024-10-09 15:19:43 +0400

    fortyfour commit

---
+++ level43/classwork/level43.js
@@ -0,0 +1,34 @@
+// davaleba 1
+const frend1 = {
+    firstname:"gio",
+    lastname:"kurtanidze",
+    age:16,
+    position:"schoolstudent",
+    hoby:cars 
+}
+frend1.firstname="nika"
+console.log(frend1.firstname)
+
+const frend2 = {
+    firstname:"gio",
+    lastname:"kurtanidze",
+    age:16,
+    position:"schoolstudent",
+    hoby:cars 
+}
+frend1.lastname=(lomidze)
+console.log(frend1.lastname)
+
+const frend3 = {
+    firstname:"gio",
+    lastname:"kurtanidze",
+    age:16,
+    position:"schoolstudent",
+    hoby:cars 
+}
+frend.age=17
+console.log(frend1.age)
+
+// davaleba 2
+console.dir(document)
+document.textContent="nika"
