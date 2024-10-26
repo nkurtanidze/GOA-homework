98bb81785f3b2b953a982374fde88ad5deda06ae
nkurtanidze <nikolozikurtanidze04@gmail.com>
2024-10-26 19:02:09 +0400

    forty eight commit

---
+++ level47/homework/level47.js
@@ -0,0 +1,28 @@
+function mcharger(minute) {
+    return minute * 60
+}
+
+function number(num1, num2, num3){
+    let firsname = num1 + num2
+    return firsname * num3
+}
+
+
+
+function isFirstLarger(n1, n1) {
+    return n1 > n2;
+}
+
+function func(num1, num2){
+    return num1>num2
+}
+
+function elementlog(element){
+    console.log("element" + element)
+}
+
+
+function task5(idd, text){
+    document.getElementById("idd").textContent = text
+}
+task5("element", "hello")
---
+++ level48/classwork/level48.js
@@ -0,0 +1,4 @@
+function htmlelement(element) {
+    console.log(element)
+}
+htmlelement(document.getElementById("element"))
